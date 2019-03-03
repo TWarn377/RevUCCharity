e04141769977a1b52b2771c4edcda9781e236c8e
Krerkkiat Chusap <krerkkiat@users.noreply.github.com>
2019-03-03 10:15:49 -0500

    Fix login problem.

--- charity/static/js/api-config.js
+++ charity/static/js/api-config.js
@@ -4,8 +4,6 @@ const hostname = window && window.location && window.location.hostname;
 
 if (hostname === 'ec2-18-235-225-4.compute-1.amazonaws.com') {
   backendHost = 'http://ec2-18-235-225-4.compute-1.amazonaws.com';
-} else if (hostname === 'ec2-18-235-225-4.compute-1.amazonaws.com') {
-  backendHost = 'https://ec2-18-235-225-4.compute-1.amazonaws.com';
 } else {
   backendHost = 'http://127.0.0.1:5000';
 }
